847d2b36c33f7d67e6671ab3ad4439bfd0e0468b
Alan Zimmerman <alan.zimm@gmail.com>
2025-07-28 22:51:35 +0800

    Progress

--- trie-map.lisp
+++ trie-map.lisp
@@ -3,7 +3,7 @@
         #:trivia)
   )
 (in-package #:trie-map)
-;; (ql:quickload "tree-matching") 
+;; (ql:quickload "tree-matching")
 ;; (in-package :trie-map)
 
 ;; ---------------------------------------------------------------------
@@ -20,13 +20,32 @@
 
 (defclass expr-map ()
   ((%em-var :accessor em-var :initform (make-hash-table :test 'equal))
+   ;; For the :app constructor, an expr-map for e1 containing an expr-map for e2
    (%em-app :accessor em-app :initform nil)
    )
   (:documentation "ExprMap v"))
 
-(defun empty-em ()
-  (make-instance 'expr-map)
+
+(defun pretty-print-expr-map (expr-map depth &optional (stream t))
+  (format stream "~v,tEXPR-MAP:~a-->>>>>>>>>>>>~%" depth expr-map)
+  ;; (format stream "PDA-alphabet:~a~%" (pda-alphabet pda))
+  ;; (format stream "PDA-states:~a~%" (reverse (pda-states pda)))
+  ;; (format stream "PDA-initial-state:~a~%" (pda-initial-state pda))
+  ;; (format stream "PDA-final-states:~a~%" (reverse (pda-final-states pda)))
+  (format stream "~v,tEXPR-MAP-em-var:~%" depth)
+  (format stream "~v,tEXPR-MAP-em-var:val:~a~%" depth (em-var expr-map))
+  (maphash (lambda (k v)
+             (format stream "~v,t   (~S . ~S)~%" depth k v))
+           (em-var expr-map))
+  (format stream "~v,tEXPR-MAP-em-app:~A~%" depth (em-app expr-map))
+  (if (not (null (em-app expr-map)))
+      (pretty-print-expr-map (em-app expr-map) (+ 2 depth)))
+  (format stream "~v,tEXPR-MAP:--<<<<<<<<<<<<<<<<<<<<~%" depth)
   )
+
+
+(defun empty-em ()
+  (make-instance 'expr-map))
 
 (defvar test-expr '(:app (:var "a") (:var "b")))
 (defvar test-em (empty-em))
@@ -53,18 +72,19 @@
         (t (format t "match failed ~%")))))
 
 (defun alter-map (f key map)
-  "Apply F to the value in the MAP at KEY (or to nil if not found).
+  "Apply F to the value in the hash-map MAP at KEY (or to nil if not found).
 Store the result in the MAP."
-  (format t "alter-map:f: ~a~%" f)
-  (format t "alter-map:key: ~a~%" key)
-  (format t "alter-map:map: ~a~%" map)
-  (setf (gethash key map) (apply f (gethash key map))))
+  ;; (format t "alter-map:f: ~a~%" f)
+  ;; (format t "alter-map:key: ~a~%" key)
+  ;; (format t "alter-map:map: ~a~%" map)
+  ;; (format t "alter-map:old-val1: ~a~%" (gethash key map))
+  (setf (gethash key map) (funcall f (gethash key map))))
 
 (defun lift-tf (f)
-  (lambda (v)
-    (if (null v)
-        (apply f (empty-em))
-        (apply f v))))
+  (lambda (em)
+    (let ((em2 (if (null em) (empty-em) em)))
+      (funcall f em2)
+      em2)))
 
 ;; m { em_app = atEM e1 (liftTF (atEM e2 tf )) app)
 (defmethod at-em (expr tf (expr-map expr-map))
@@ -74,12 +94,49 @@ Store the result in the MAP."
       expr-map
     (format t "at-em:em-var: ~a~%" em-var)
     (format t "at-em:em-app: ~a~%" em-app)
+    (pretty-print-expr-map expr-map 0)
     (match expr
-      ((list :var v) (alter-map tf v em-var))
+      ((list :var v)
+
+       (format t "at-em:processing :var: ~a~%" v)
+       (alter-map tf v em-var))
       ((list :app e1 e2)
-       (let ((f2 (lift-tf (lambda (m) (at-em e2 tf m)))))
-         (setf em-app (at-em e1 f2 em-app))))
-      (t (format t "match failed ~%")))))
+       ;; App e1 e2 → m { em_app = atEM e1 (liftTF (atEM e2 tf )) app }
+       (format t "at-em:processing :app: ~a ~a~%" e1 e2)
+       ;; ------------------------------
+       ;; We want the current expr-map to have a new or updated one, indexed by e1.
+       ;; This one must have an entry for e2, which is updated by TF.
+       (let* ((em2 (if (null em-app) (empty-em) em-app))
+              (em2-val (at-em e2 tf em2)))
+         (format t "at-em:em-app: ~a~%" em-app)
+         (format t "at-em:em2: ~a~%" em2)
+         (pretty-print-expr-map em2 0)
+         (format t "at-em:em2-val: ~a~%" em2-val)
+         (format t "at-em:app:recursing-----------------------------~%")
+         (setf em-app (at-em e1 tf em2))
+         (format t "at-em:em-app: ~a~%" em-app)
+         ))
+
+       ;; ------------------------------
+       ;; (let* ((f2 (lift-tf (lambda (em) (at-em e2 tf em))))
+       ;;        ;; (em2 (at-em e1 f2 em-app)))
+       ;;        (em2 nil))
+       ;;   (format t "at-em:em2: ~a~%" em2)
+       ;;   (pretty-print-expr-map em2)
+       ;;   (setf em-app (at-em e1 f2 em-app))
+       ;;   (pretty-print-expr-map em-app)))
+       ;; ------------------------------
+       ;; (let ((em2 (funcall (lift-tf (lambda (em) (at-em e2 tf em))) em-app)))
+       ;;   (format t "at-em:em-app: ~a~%" em-app)
+       ;;   (format t "at-em:em2: ~a~%" em2)
+       ;;   (pretty-print-expr-map em2)
+       ;;   (at-em e1 tf em2)
+       ;;   (format t "at-em:em2:2: ~a~%" em2)
+       ;;   (pretty-print-expr-map em2)
+       ;;   ))
+       ;; ------------------------------
+      (t (format t "match failed ~%")))
+    expr-map))
 
 (defmethod insert-em (expr v (expr-map expr-map))
   "Insert V as the value for key EXPR in EXPR-MAP."
@@ -90,7 +147,7 @@ Store the result in the MAP."
 
 (defmethod delete-em (expr (expr-map expr-map))
   "Delete the entry for EXPR in EXPR-MAP."
-  (at-em expr (lambda (x)
+  (at-em expr #'(lambda (x)
                 (declare (ignore x))
                 nil)
          expr-map))
@@ -111,4 +168,14 @@ Store the result in the MAP."
   (lk-em test-expr test-em))
 
 (defun t2 ()
-  (insert-em '(:var "x") 'v (empty-em)))
+  (setf test-em (empty-em))
+  (insert-em '(:var "x") 'v test-em)
+  (format t "test-em: ~a~%" test-em)
+  (pretty-print-expr-map test-em 0))
+
+(defun t3 ()
+  (setf test-em (empty-em))
+  (insert-em '(:app (:var "x") (:var "y")) 'inserted-val test-em)
+  (format t "test-em:--------------------~%")
+  (format t "test-em: ~a~%" test-em)
+  (pretty-print-expr-map test-em 0))
